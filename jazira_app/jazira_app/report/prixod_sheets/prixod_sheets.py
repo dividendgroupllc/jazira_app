ed007b5f299d8b62b3e5faa48354d1a9ecd694ab
sar552 <sardorbekqamchibekov76@gmail.com>
2026-06-14 18:26:49 +0500

    new report

---
+++ jazira_app/jazira_app/report/prixod_sheets/prixod_sheets.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2026, Jazira App and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.utils import flt, getdate
+
+
+def execute(filters=None):
+    filters = filters or {}
+    validate_filters(filters)
+    columns = get_columns()
+    data = get_data(filters)
+    report_summary = get_report_summary(data)
+    return columns, data, None, None, report_summary
+
+
+def validate_filters(filters):
+    if not filters.get("from_date") or not filters.get("to_date"):
+        frappe.throw(_("Сана оралиғи мажбурий (from_date / to_date)"))
+    if getdate(filters["from_date"]) > getdate(filters["to_date"]):
+        frappe.throw(_("Бошланиш санаси тугаш санасидан катта бўлиши мумкин эмас"))
+
+
+def get_columns():
+    return [
+        {"fieldname": "purchase_invoice", "label": _("Ҳужжат"), "fieldtype": "Link", "options": "Purchase Invoice", "width": 170},
+        {"fieldname": "posting_date", "label": _("Сана"), "fieldtype": "Date", "width": 100},
+        {"fieldname": "company", "label": _("Компания"), "fieldtype": "Link", "options": "Company", "width": 150},
+        {"fieldname": "supplier", "label": _("Етказиб берувчи"), "fieldtype": "Data", "width": 180},
+        {"fieldname": "item_name", "label": _("Товар номи"), "fieldtype": "Data", "width": 240},
+        {"fieldname": "qty", "label": _("Дона"), "fieldtype": "Float", "width": 100, "precision": 3},
+        {"fieldname": "rate", "label": _("Нарxи"), "fieldtype": "Currency", "width": 120},
+        {"fieldname": "amount", "label": _("Суммаси"), "fieldtype": "Currency", "width": 140},
+    ]
+
+
+def get_data(filters):
+    conditions = ["pi.docstatus = 1", "pi.posting_date BETWEEN %(from_date)s AND %(to_date)s"]
+    params = {"from_date": filters["from_date"], "to_date": filters["to_date"]}
+
+    if filters.get("company"):
+        conditions.append("pi.company = %(company)s")
+        params["company"] = filters["company"]
+    if filters.get("supplier"):
+        conditions.append("pi.supplier = %(supplier)s")
+        params["supplier"] = filters["supplier"]
+
+    where = " AND ".join(conditions)
+
+    rows = frappe.db.sql(f"""
+        SELECT
+            pi.name AS purchase_invoice,
+            pi.posting_date,
+            pi.company,
+            IFNULL(pi.supplier_name, pi.supplier) AS supplier,
+            pii.item_name,
+            pii.qty,
+            pii.rate,
+            pii.amount
+        FROM `tabPurchase Invoice Item` pii
+        INNER JOIN `tabPurchase Invoice` pi ON pi.name = pii.parent
+        WHERE {where}
+        ORDER BY pi.posting_date, pi.name, pii.idx
+    """, params, as_dict=True)
+
+    if rows:
+        total_qty = sum(flt(r.qty) for r in rows)
+        total_amount = sum(flt(r.amount) for r in rows)
+        rows.append({
+            "purchase_invoice": None,
+            "posting_date": None,
+            "company": None,
+            "supplier": None,
+            "item_name": _("ЖАМИ"),
+            "qty": total_qty,
+            "rate": None,
+            "amount": total_amount,
+        })
+
+    return rows
+
+
+def get_report_summary(data):
+    total_amount = 0
+    total_qty = 0
+    for r in data:
+        # jami qatorni hisobga olmaymiz (item_name == 'ЖАМИ')
+        if r.get("item_name") == _("ЖАМИ"):
+            continue
+        total_amount += flt(r.get("amount"))
+        total_qty += flt(r.get("qty"))
+
+    return [
+        {"label": _("Жами сумма"), "value": total_amount, "datatype": "Currency", "indicator": "Green"},
+        {"label": _("Қаторлар сони"), "value": max(len(data) - 1, 0) if data else 0, "datatype": "Int"},
+    ]
